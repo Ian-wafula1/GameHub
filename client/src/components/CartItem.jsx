8c1452ef46a0ab4a09685628fe99ae91a3c5ff3f
Ian Wafula <neocorpse@proton.me>
2025-06-29 08:34:43 +0300

    Complete cart styling

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,9 +1,11 @@
 export default function CartItem({item}) {
     return (
-        <div>
-            <img src={item?.img_url || "https://placehold.co/400/grey/black"} style={{width:'50px', height:'50px'}} alt={item.name} />
-            <h1>{item?.name}</h1>
-            <p>Price: {item?.price}</p>
+        <div className="bg-neutral-800 rounded-2xl flex overflow-clip gap-3 pr-4 h-[5.8rem]">
+            <img className="h-[100%]" src={item?.img_url || "https://placehold.co/400/grey/black"}  alt={item.name} />
+            <div className="my-2 flex flex-col justify-between">
+                <h1 className="font-bold text-xl">{item?.name}</h1>
+                <p className="font-bold text-green-700">{'$'+item?.price || 'Unlisted'}</p>
+            </div>
         </div>
     )
 }
