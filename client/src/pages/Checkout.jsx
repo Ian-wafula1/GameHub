4d954a483b76510d3794d0368f57be94f6827cae
Ian Wafula <neocorpse@proton.me>
2025-06-29 09:43:15 +0300

    Fix bug with video not changing with page change

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,51 +1,60 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
-import confirm from "../utils/confirmLogin"
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import confirm from '../utils/confirmLogin';
 
 export default function Checkout() {
-    confirm() ? true : navigate('/login')
-    const navigate = useNavigate()
+	confirm() ? true : navigate('/login');
+	const navigate = useNavigate();
 
-    const [cartItems, setCartItems] = useState([])
+	const [cartItems, setCartItems] = useState([]);
 
-    useEffect(() => {
-        axios.get('/api/cart', {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        }).then(res => {
-            setCartItems(res.data)
-        }).catch(err => console.log(err))
-    }, [])
+	useEffect(() => {
+		axios
+			.get('/api/cart', {
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+			})
+			.then((res) => {
+				setCartItems(res.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
-    function confirmPurchase() {
-        axios.post('/api/orders', {
-            items: cartItems,
-            total: cartItems.reduce((acc, item) => acc + item.price, 0)
-        }, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-        navigate('/orders')
-    }
-    return (
-        <>
-        <div>
-            {cartItems.map(item => {
-                return <div key={item.id}>
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                </div>
-            })}
-        </div>
-        <div>
-            <p>Total: {cartItems.reduce((acc, item) => acc + item.price, 0)}</p>
-        </div>
-        <div>
-            <button onClick={confirmPurchase} >Confirm Purchase</button>
-        </div>
-        </>
-    )
+	function confirmPurchase() {
+		axios.post(
+			'/api/orders',
+			{
+				items: cartItems,
+				total: cartItems.reduce((acc, item) => acc + item.price, 0),
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+			}
+		);
+		navigate('/orders');
+	}
+	return (
+		<div className="text-white">
+			<div>
+				{cartItems.map((item) => {
+					return (
+						<div key={item.id}>
+							<p>{item.name}</p>
+							<p>{item.price}</p>
+						</div>
+					);
+				})}
+			</div>
+			<div>
+				<p>Total: {cartItems.reduce((acc, item) => acc + item.price, 0)}</p>
+			</div>
+			<div>
+				<button onClick={confirmPurchase}>Confirm Purchase</button>
+			</div>
+		</div>
+	);
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,58 +4,64 @@ import { MyCheckbox, MySelect, MyTextInput } from '../utils/formElements';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import vid from '../assets/video1.mp4';
 
 export default function Login() {
 	const navigate = useNavigate();
 	return (
-		<div className="m-5 flex flex-col max-w-[580px] w-[80%] px-8 py-5 self-center my-20 bg-[#ffffff33] text-white text-lg font-semibold backdrop-blur-[10px] rounded-3xl ">
-			<h1 className="text-3xl self-center mb-5">Log in</h1>
-			<Formik
-				initialValues={{
-					username: '',
-					password: '',
-				}}
-				validationSchema={Yup.object({
-					username: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-					password: Yup.string().min(8, 'Must be 8 characters or more').required('Required'),
-				})}
-				onSubmit={(values, { setSubmitting }) => {
-					axios
-						.post(`/api/login`, {
-							username: values.username,
-							password: values.password,
-						})
-						.then((response) => {
-							let token = response.data.access_token;
-							localStorage.setItem('token', token);
-							navigate('/');
-						})
-						.catch((error) => console.error(error))
-						.finally(() => {
-							setSubmitting(false);
-						});
-				}}>
-
-				<Form className='flex flex-col gap-2 '>
-					<MyTextInput name="username" type="text" label="Username" />
-					<MyTextInput name="password" type="password" label="Password" />
-					<button className="my-5 border-2 border-white bg-gray-700 transition-colors rounded-3xl px-4 py-[.6rem] hover:bg-slate-900  w-60 self-center" type="submit">
-						Login
-					</button>
-				</Form>
-			</Formik>
-			<p className='text-slate-900'>
-				Don't have an account?{' '}
-				<Link to="/signup" className="text-purple-900 underline">
-					Sign up
-				</Link>
-			</p>
-			<p className='text-slate-900'>
-				Forgot your password?{' '}
-				<Link to="/reset-password" className="text-purple-900 underline">
-					Reset password
-				</Link>
-			</p>
-		</div>
+		<>
+			<video className="fixed z-[-1] w-screen h-screen inset-0 max-w-none opacity-100 transform-none object-cover " autoPlay loop muted playsInline>
+				<source src={vid} type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+			<div className="m-5 flex flex-col max-w-[580px] w-[80%] px-8 py-5 self-center my-20 bg-[#ffffff33] text-white text-lg font-semibold backdrop-blur-[10px] rounded-3xl ">
+				<h1 className="text-3xl self-center mb-5">Log in</h1>
+				<Formik
+					initialValues={{
+						username: '',
+						password: '',
+					}}
+					validationSchema={Yup.object({
+						username: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+						password: Yup.string().min(8, 'Must be 8 characters or more').required('Required'),
+					})}
+					onSubmit={(values, { setSubmitting }) => {
+						axios
+							.post(`/api/login`, {
+								username: values.username,
+								password: values.password,
+							})
+							.then((response) => {
+								let token = response.data.access_token;
+								localStorage.setItem('token', token);
+								navigate('/');
+							})
+							.catch((error) => console.error(error))
+							.finally(() => {
+								setSubmitting(false);
+							});
+					}}>
+					<Form className="flex flex-col gap-2 ">
+						<MyTextInput name="username" type="text" label="Username" />
+						<MyTextInput name="password" type="password" label="Password" />
+						<button className="my-5 border-2 border-white bg-gray-700 transition-colors rounded-3xl px-4 py-[.6rem] hover:bg-slate-900  w-60 self-center" type="submit">
+							Login
+						</button>
+					</Form>
+				</Formik>
+				<p className="text-slate-900">
+					Don't have an account?{' '}
+					<Link to="/signup" className="text-purple-900 underline">
+						Sign up
+					</Link>
+				</p>
+				<p className="text-slate-900">
+					Forgot your password?{' '}
+					<Link to="/reset-password" className="text-purple-900 underline">
+						Reset password
+					</Link>
+				</p>
+			</div>
+		</>
 	);
 }
